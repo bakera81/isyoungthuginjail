bd0af68158bb7e15631b3d5148216151749795ca
Anthony Baker <anthonybaker@anthonys-mbp-2.mynetworksettings.com>
2022-05-19 10:58:32 -0400

    init

---
+++ gatsby-config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  siteMetadata: {
+    title: `Is Young Thug in Jail`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-emotion", {
+    resolve: 'gatsby-plugin-google-analytics',
+    options: {
+      "trackingId": "G-2XCXT0BYTB"
+    }
+  }, "gatsby-plugin-react-helmet"]
+};
---
+++ src/pages/index.js
@@ -0,0 +1,31 @@
+import * as React from "react"
+
+// styles
+const pageStyles = {
+  color: "#232129",
+  padding: 96,
+  fontFamily: "-apple-system, Roboto, sans-serif, serif",
+}
+const headingStyles = {
+  marginTop: 0,
+  marginBottom: 64,
+  maxWidth: 320,
+}
+
+
+// data
+
+
+// markup
+const IndexPage = () => {
+  return (
+    <main style={pageStyles}>
+      <title>Is Young Thug in Jail?</title>
+      <h1 style={headingStyles}>
+        No.
+      </h1>
+    </main>
+  )
+}
+
+export default IndexPage
